db1b9c09f64c4ad7ef2953ae9c1126074af638d7
MorStaWid <mswidjaj@uci.edu>
2024-07-02 15:32:05 -0700

    Changed Color Line and Remove Border sides

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import StatsComponent from "@/components/StatsComponent";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <StatsComponent />
     </main>
   );
 }
